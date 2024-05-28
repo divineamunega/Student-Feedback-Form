fc3f94775c48f36ca51dc5f11d85180287734d09
divineamunega <divineamunega@gmail.com>
2024-05-28 18:08:02 +0100

    =Fix: Fixed the redirect navigate route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Page from "./components/Page";
+import { Navigate } from "react-router-dom";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route index path="/signup" element={<SignUp />} />
+				<Route index path="/" element={<Navigate to="/signup" />} />
+				<Route path="/signup" element={<SignUp />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/page" element={<Page />} />
 			</Routes>
