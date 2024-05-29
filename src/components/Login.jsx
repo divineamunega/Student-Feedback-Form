34e04a73dcc7156c53b3c34cb56bf38745603a06
MhideTech <okefolahanolamide2006@gmail.com>
2024-05-29 12:30:14 +0100

    Feat: Designed the feedback page and changes the background image and colors of the signup page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,9 @@
 import styles from "./SignUp.module.css";
 import { useState } from "react";
 import {
-	signInWithEmailAndPassword,
-	setPersistence,
-	browserLocalPersistence,
+  signInWithEmailAndPassword,
+  setPersistence,
+  browserLocalPersistence,
 } from "firebase/auth";
 import { auth } from "../firebase";
 import { Link } from "react-router-dom";
@@ -11,64 +11,64 @@ import { ToastContainer, toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const navigate = useNavigate();
-	async function signUp(e) {
-		e.preventDefault();
-		try {
-			if (email === "" || password === "")
-				throw new Error("Please Fill all the required feilds.");
-			await setPersistence(auth, browserLocalPersistence).then(() =>
-				signInWithEmailAndPassword(auth, email, password)
-			);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  async function signUp(e) {
+    e.preventDefault();
+    try {
+      if (email === "" || password === "")
+        throw new Error("Please Fill all the required feilds.");
+      await setPersistence(auth, browserLocalPersistence).then(() =>
+        signInWithEmailAndPassword(auth, email, password)
+      );
 
-			navigate("/page");
-		} catch (err) {
-			toast.error(err.message);
-			// setName("");
-			// setEmail("");
-			// setPassword("");
-		}
-	}
+      navigate("/page");
+    } catch (err) {
+      toast.error(err.message);
+      // setName("");
+      // setEmail("");
+      // setPassword("");
+    }
+  }
 
-	return (
-		<>
-			<ToastContainer />
-			<form onSubmit={signUp} className={styles.formBox}>
-				<h1>Welcome!</h1>
-				<h2>Nice to meet you again</h2>
-				<div className={styles.inputGroup}>
-					<div className={styles.inputField}>
-						<input
-							type="email"
-							placeholder="Email"
-							className="email"
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-					</div>
-					<div className={styles.inputField}>
-						<input
-							type="password"
-							placeholder="Password"
-							className="password"
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-						/>
-					</div>
-					<p>
-						${"Don't have an account"} <Link to="/auth/signup">Sign Up!</Link>
-					</p>
-				</div>
-				<div className={styles.btnField}>
-					<button type="submit" className={styles.actionBtn}>
-						Sign In
-					</button>
-				</div>
-			</form>
-		</>
-	);
+  return (
+    <>
+      <ToastContainer />
+      <form onSubmit={signUp} className={styles.formBox}>
+        <h1>Welcome!</h1>
+        <h2>Nice to meet you again</h2>
+        <div className={styles.inputGroup}>
+          <div className={styles.inputField}>
+            <input
+              type="email"
+              placeholder="Email"
+              className="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className={styles.inputField}>
+            <input
+              type="password"
+              placeholder="Password"
+              className="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <p>
+            {"Don't have an account"} <Link to="/auth/signup">Sign Up!</Link>
+          </p>
+        </div>
+        <div className={styles.btnField}>
+          <button type="submit" className={styles.actionBtn}>
+            Sign In
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default Login;
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -4,21 +4,49 @@ import { auth } from "../firebase";
 import { onAuthStateChanged } from "firebase/auth";
 
 function Page() {
-	const [name, setName] = useState("");
-	onAuthStateChanged(auth, (user) => {
-		if (user) {
-			// User is signed in, see docs for a list of available properties
-			// https://firebase.google.com/docs/reference/js/firebase.User
-			setName(user.displayName);
-			// ...
-		} else {
-			// User is signed out
-			console.log("Use Is signed out");
-			// ...
-		}
-	});
-	console.log(styles.page);
-	return <div className={styles.page}>Welcome {name}</div>;
+  const [name, setName] = useState("");
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      setName(user.displayName);
+      // ...
+    } else {
+      // User is signed out
+      console.log("Use Is signed out");
+      // ...
+    }
+  });
+  return (
+    <div className={styles.page}>
+      <header className={styles.header}>Welcome {name}!</header>
+      <div className={styles.app}>
+        <form action="">
+          <input
+            type="text"
+            name="lecturer-name"
+            id="lecturer-name"
+            placeholder="Lecturer's Name"
+          />
+          <input
+            type="text"
+            name="course-code"
+            id="course-code"
+            placeholder="Course Code"
+          />
+          <input
+            type="text"
+            name="department"
+            id="department"
+            placeholder="Department"
+          />
+          <input type="text" name="level" id="level" placeholder="Level" />
+          <textarea name="feedback" id="feedback" placeholder="Enter your Feedback"></textarea>
+          <button type="submit">Send Feedback</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
